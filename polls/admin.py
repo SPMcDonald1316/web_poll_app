5c593437228399fb361fd31c779f4c80f5d873c3
Sean McDonald <spmcdonald1316@gmail.com>
2020-08-22 12:40:09 -0500

    add pub_date filter and question_text search

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
 class ChoiceInline(admin.TabularInline):
   model = Choice
@@ -12,5 +12,7 @@ class QuestionAdmin(admin.ModelAdmin):
   ]
   inlines = [ChoiceInline]
   list_display = ('question_text', 'pub_date', 'was_published_recently')
+  list_filter = ['pub_date']
+  search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
